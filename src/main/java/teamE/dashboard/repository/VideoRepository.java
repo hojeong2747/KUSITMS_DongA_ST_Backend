fffa4d2e012524d4fe38dad79ccba84773aad651
hojeong2747 <hojeong2747@gmail.com>
2023-03-11 19:55:27 +0900

    add: videoContents

---
+++ src/main/java/teamE/dashboard/repository/VideoRepository.java
@@ -0,0 +1,9 @@
+package teamE.dashboard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import teamE.dashboard.entity.Video;
+
+public interface VideoRepository extends JpaRepository<Video, Long> {
+
+
+}
