c6d283b44869126635b7eff3e0950314c63e8d4b
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-03-13 01:39:02 +0900

    #2 AWS S3 이미지 불러오기 테스트 성공

--- src/main/java/teamE/dashboard/DashboardApplication.java
+++ src/main/java/teamE/dashboard/DashboardApplication.java
@@ -3,7 +3,11 @@ package teamE.dashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+        })
 public class DashboardApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/teamE/dashboard/security/AwsS3Service.java
@@ -0,0 +1,20 @@
+package teamE.dashboard.security;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AwsS3Service {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    public String getThumbnailPath(String path) {
+        return amazonS3Client.getUrl(bucketName, path).toString();
+    }
+}
