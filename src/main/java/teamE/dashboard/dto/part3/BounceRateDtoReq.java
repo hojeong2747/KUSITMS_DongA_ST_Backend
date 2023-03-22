46166090481a6ebe02e75b6cdbf2fac990863d80
hojeong2747 <hojeong2747@gmail.com>
2023-03-22 20:58:15 +0900

    [fix/part3] swagger info

--- src/main/java/teamE/dashboard/dto/part3/BounceRateDtoReq.java
+++ src/main/java/teamE/dashboard/dto/part3/BounceRateDtoReq.java
@@ -1,5 +1,6 @@
 package teamE.dashboard.dto.part3;
 
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BounceRateDtoReq {
+    @ApiParam(value = "날짜", type = "String", required = true, example = "2023-03-15")
     private String date;
 }
--- src/main/java/teamE/dashboard/dto/part3/PageViewDtoReq.java
+++ src/main/java/teamE/dashboard/dto/part3/PageViewDtoReq.java
@@ -1,5 +1,6 @@
 package teamE.dashboard.dto.part3;
 
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PageViewDtoReq {
+    @ApiParam(value = "카테고리", type = "int", required = true, example = "4")
     private int category;
+    @ApiParam(value = "날짜", type = "String", required = true, example = "2023-03-15")
     private String date;
 }
