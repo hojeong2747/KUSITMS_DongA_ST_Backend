44b3b6acc95d04ec91120beabf506b3d85741091
hojeong2747 <hojeong2747@gmail.com>
2023-03-22 21:57:09 +0900

    [fix/part3] pageView, bounceRate 수정 완료

--- src/main/java/teamE/dashboard/controller/BounceRateController.java
+++ src/main/java/teamE/dashboard/controller/BounceRateController.java
@@ -17,6 +17,7 @@ import teamE.dashboard.dto.part3.BounceRateDtoRes;
 import teamE.dashboard.entity.BounceRate;
 import teamE.dashboard.service.BounceRateService;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,6 +38,7 @@ public class BounceRateController {
         List<BounceRateDtoRes> res = findBounceRates.stream()
                 .map(m -> new BounceRateDtoRes(Integer.parseInt(m.getDate().substring(8)), m.getExitUserCount(), m.getTotalUserCount(), Double.valueOf(String.format("%.02f", (((double)m.getExitUserCount()/m.getTotalUserCount()) * 100)))))
                 .collect(Collectors.toList());
+//        Collections.sort(res);
 
         return new ResponseEntity<>(new Result(res), HttpStatus.OK);
     }
